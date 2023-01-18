ccf9fa748188f802fc794cadbdd926b608e58c44
jere-mie <jeremiedevelops@gmail.com>
2023-01-18 15:05:18 -0500

    combined a11 and a12 endpoints

--- website/routes.py
+++ website/routes.py
@@ -12,80 +12,38 @@ def home():
 def a1():
     return render_template('a1.html')
 
-@app.route('/run/a11', methods=['POST'])
-def a11():
+@app.route('/run/a1/<id>', methods=['POST'])
+def runa1(id):
 
     code = request.get_json()['code']
 
     # create the program
-    with open('website/static/java/A11_template.java', 'r') as f:
-        A11template = f.read()
+    with open(f'website/static/java/A1{id}_template.java', 'r') as f:
+        A1template = f.read()
     
-    with open('website/static/java/A11.java', 'w') as f:
-        f.write(A11template.replace('//IDENTIFIERCODEHERE', code).replace('A11_template', 'A11'))
+    with open('website/static/java/A1.java', 'w') as f:
+        f.write(A1template.replace('//IDENTIFIERCODEHERE', code).replace(f'A1{id}_template', 'A1'))
 
     # compile the program
     try:
-        os.system('rm website/static/java/A11.class')
-        subprocess.run(['javac', 'website/static/java/A11.java'])
-        if not os.path.exists('website/static/java/A11.class'):
+        os.system('rm website/static/java/A1.class')
+        subprocess.run(['javac', 'website/static/java/A1.java'])
+        if not os.path.exists('website/static/java/A1.class'):
             raise Exception()
     except:
-        os.system('rm website/static/java/A11.java')
+        os.system('rm website/static/java/A1.java')
         return "Your java program would not compile. 0 marks received."
 
     # run the program
     try:
-        output = subprocess.check_output(['java', 'A11'], cwd='website/static/java', timeout=2).decode()
+        output = subprocess.check_output(['java', 'A1'], cwd='website/static/java', timeout=2).decode()
     except:
-        os.system('rm website/static/java/A11.class')
-        os.system('rm website/static/java/A11.java')
+        os.system('rm website/static/java/A1.class')
+        os.system('rm website/static/java/A1.java')
         return "Your java program compiled, but had a runtime error. 1 mark received."
 
-    os.system('rm website/static/java/A11.class')
-    os.system('rm website/static/java/A11.java')
-
-    # program ran without error
-    counts = [5, 4, 6, 7, 8, 9]
-    out = '<ul>'
-    output = output.split('\n')
-    for i in range(len(output)-1):
-        out+=f'<li>case {i+1} | received: {int(output[i]):02} | expected: {counts[i]:02}</li>'
-    out+='</ul>'
-    return out
-
-@app.route('/run/a12', methods=['POST'])
-def a12():
-
-    code = request.get_json()['code']
-
-    # create the program
-    with open('website/static/java/A12_template.java', 'r') as f:
-        A12template = f.read()
-    
-    with open('website/static/java/A12.java', 'w') as f:
-        f.write(A12template.replace('//IDENTIFIERCODEHERE', code).replace('A12_template', 'A12'))
-
-    # compile the program
-    try:
-        os.system('rm website/static/java/A12.class')
-        subprocess.run(['javac', 'website/static/java/A12.java'])
-        if not os.path.exists('website/static/java/A12.class'):
-            raise Exception()
-    except:
-        os.system('rm website/static/java/A12.java')
-        return "Your java program would not compile. 0 marks received."
-
-    # run the program
-    try:
-        output = subprocess.check_output(['java', 'A12'], cwd='website/static/java', timeout=2).decode()
-    except:
-        os.system('rm website/static/java/A12.class')
-        os.system('rm website/static/java/A12.java')
-        return "Your java program compiled, but had a runtime error. 1 mark received."
-
-    os.system('rm website/static/java/A12.class')
-    os.system('rm website/static/java/A12.java')
+    os.system('rm website/static/java/A1.class')
+    os.system('rm website/static/java/A1.java')
 
     # program ran without error
     counts = [5, 4, 6, 7, 8, 9]
