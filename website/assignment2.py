c36de16ec5780e036608f151724d10f03d716973
Usman <farooqiu@uwindsor.ca>
2023-01-28 02:50:26 -0500

    fixed a2 to show errors and prevent crashes

--- website/assignment2.py
+++ website/assignment2.py
@@ -1,18 +1,33 @@
 import os
-import secrets
+import uuid
+import shutil
 class assignment:
     def __init__(self, subprocess, code):
         self.subprocess = subprocess
         self.code = code
+    
+    def remove_submission(self, unique_dir, rootdir): 
+        os.chdir('../')
+        os.system(f'rm -rf {unique_dir}')
+        os.chdir(rootdir)
+
 
     def run_a21(self):
         # store the root directory and change to assignment directory
         rootdir = os.getcwd()
         os.chdir("website/static/java/Assignment2/A21")
-        unique_id = secrets.token_hex()
+        unique_id = uuid.uuid1()
+        unique_dir = f'Simulator_{unique_id}'
+        # make a unique directory
+        os.mkdir(unique_dir)
+        os.system(f'cp DFA.java {unique_dir}')
+        os.system(f'cp Simulator_template.java {unique_dir}')
+
         with open(f'Simulator_template.java', 'r') as f:
             A2template = f.read()
         
+        os.chdir(unique_dir)
+
         with open('Simulator.java', 'w') as f:
             f.write(A2template.replace('//INSERTCODEHERE', self.code))
 
@@ -25,27 +40,31 @@ class assignment:
                 raise Exception()
         except:
             print(error)
-            os.system('rm Simulator.java')
+            self.remove_submission(unique_dir, rootdir)
             return "DFA.java failed to compile contact server owner to check for logs"
 
         # run the program
         dfa_map = {}
         expected_dfa = {}
+        error = ""
         for i in range(4):
             try:
-                output = self.subprocess.check_output(['java', 'DFA', 'dfa{}.txt'.format(i), 'dfa{}.input.txt'.format(i), 'output.txt'], cwd='.', timeout=2).decode()
+                command = "java DFA ../dfa{0}.txt ../dfa{0}.input.txt output.txt".format(i)
+                proc = self.subprocess.Popen(command, shell=True, stdout=self.subprocess.PIPE, stderr=self.subprocess.PIPE)
+                stdout, stderr = proc.communicate()
+                error = stderr.decode('utf-8')
+                if error != "":
+                    raise Exception()
             except:
-                os.system('rm DFA.class')
-                os.system('rm Simulator.java')
-                os.system('rm output.txt')
-                return "Your java program compiled, but either had a runtime error or ran for too long. 1 mark received."
+                self.remove_submission(unique_dir, rootdir)
+                return f"Your java program compiled, but either had a runtime error or ran for too long. 1 mark received.<br> The following is the error:<br><br>{error}"
 
             with open(f'output.txt', 'r') as f:
                 outputs = f.read().split('\n')[:-1]
 
             dfa_map['dfa {}'.format(i)] = outputs
 
-            with open('answer_dfa{}.txt'.format(i), 'r') as f:
+            with open('../answer_dfa{}.txt'.format(i), 'r') as f:
                 answers = f.read().split('\n')[:-1]
                 
             expected_dfa['dfa {}'.format(i)] = answers
@@ -59,9 +78,6 @@ class assignment:
                 out+= f'<br>'
         else:
             out+= f'<li> Error in parsing received outputs and expected outputs'
-
-        os.system('rm *.class')
-        os.system('rm Simulator.java')
-        os.chdir(rootdir)
+        self.remove_submission(unique_dir, rootdir)
         return out
 
