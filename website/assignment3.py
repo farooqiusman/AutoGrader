4866a5c298961140dd11246f01d5c4200f654e25
Usman <farooqiu@uwindsor.ca>
2023-02-09 18:32:29 -0500

    fixed URGENT error with not getting A3.lex file

--- website/assignment3.py
+++ website/assignment3.py
@@ -1,8 +1,8 @@
 import os
 import uuid
 import json
-from .assign3Files.assign3cup import a3cup
-from .assign3Files.assign3lex import a3lex
+import time
+from .helper import static_methods
 from mysql.connector import connect, Error
 from dotenv import load_dotenv
 
@@ -44,28 +44,18 @@ class assignment3:
         except:
             return "", ""
 
-    def run_process(self, command, directory):
-        print(command)
-        proc = self.subprocess.Popen(command, shell=True, cwd=directory, stdout=self.subprocess.PIPE, stderr=self.subprocess.PIPE)
-        error = ""
+    def generate_file_from_template(self, template_file, generated_file, code):
         out = ""
         try:
-            stdout, stderr = proc.communicate(timeout=15)
-            error = stderr.decode('utf-8')
-            print(error)
-            if error != "":
-                raise Exception()
-        except self.subprocess.TimeoutExpired:
-            proc.kill()
-            out += f"Error running: <code>{command}</code> took too long to run. <br>"
-            # self.remove_submission(directory)
-            return out
-        except:
-            # self.remove_submission(directory)
-            formatted_error = error.replace("\n", "<br>")
-            out += f"Error running command: <code>{command}</code> <br><br>{formatted_error}"
-            return out
-        out += stdout.decode('utf-8').replace("\n", "<br>")
+            with open(template_file, 'r') as f:
+                A3template = f.read()
+            
+            with open(generated_file, 'w') as f:
+                f.write(A3template.replace('//INSERTCODEHERE', code))
+        except Exception as e:
+            out += f'File error {e}<br>'
+            return False, out
+        
         return True, out
 
     def run_a3(self):
@@ -99,34 +89,66 @@ class assignment3:
 
         ###########################  Prepare Lex file ###########################
         assignment_dir = f"website/static/java/Assignment3"
-        unique_dir = f'{assignment_dir}/assignment3_{self.stduID}'
-
-        os.mkdir(unique_dir)
+        user_dir = f'assignment3_{self.stduID}'
+        unique_dir = f'{assignment_dir}/{user_dir}'
+        
+        if os.path.exists(unique_dir):
+            self.remove_submission(unique_dir)
+            print("Cleaning up working DIR")
+            time.sleep(1)
+            os.mkdir(unique_dir)
+        else:
+            os.mkdir(unique_dir)
 
         os.system(f'cp {assignment_dir}/a3_template.lex {unique_dir}')
-
-        with open(f'{unique_dir}/a3_template.lex', 'r') as f:
-            A3template = f.read()
         
-        with open(f'{unique_dir}/A3.lex', 'w') as f:
-            f.write(A3template.replace('//INSERTCODEHERE', self.lex_code))
-
-        print("Created A3.lex")
-        ###########################  Run Lex file ###########################
-        out = "Creating DFA from Lex file...<br>"
-        command = f'java JLex.Main assignment3_{self.stduID}/A3.lex'
-        ret, perror = self.run_process(command, assignment_dir)
+        ret, perror = self.generate_file_from_template(f'{unique_dir}/a3_template.lex', f'{unique_dir}/A3.lex', self.lex_code)
         if not ret:
             out += perror
-            # self.remove_submission(unique_dir)
+            self.remove_submission(unique_dir)
+            return out.replace('\n', '<br>')
+
+        print("Created A3.lex")
+
+        ###########################  Run Lex file ###########################
+        out = "Creating DFA from Lex file...<br>"
+        command = f'java JLex.Main {user_dir}/A3.lex'
+        ret, perror = static_methods.run_process(self.subprocess, command, assignment_dir)
+        if not ret:
+            out += perror
+            self.remove_submission(unique_dir)
             return out.replace("\n", "<br>")
         elif "Error" in perror:
             out += f"You have an error in your lex file please check the output below.<br><code>{perror}</code><br>"
-            # self.remove_submission(unique_dir)
+            self.remove_submission(unique_dir)
             return out
         else:
             out += f'<code>{perror}</code>'
     
         print("JLex Ran fine")
+
+        ###########################  Generate files using Cup file ###########################
+        os.system(f'cp {assignment_dir}/a3_template.cup {unique_dir}')
+        ret, perror = self.generate_file_from_template(f'{unique_dir}/a3_template.cup', f'{unique_dir}/A3.cup', self.cup_code)
+        if not ret:
+            out += perror
+            self.remove_submission(unique_dir)
+            return out.replace('\n', '<br>')
+
+        out += "Generated A3.lex.java<br>"
+        if self.cup_code == "":
+            self.remove_submission(unique_dir)
+            out += "You did not submit a cup file.\n"
+            return out.replace('\n', '<br>')
+        command = f'java java_cup.Main -parser {user_dir}/A3Parser -symbols {user_dir}/A3Symbol < {user_dir}/A3.cup'
+        ret, perror = static_methods.run_process(self.subprocess, command, assignment_dir)
+        print(ret)
+        if not ret:
+            out += perror
+            self.remove_submission(unique_dir)
+            return out.replace('\n', '<br>')
+        print("Command ran fine")
+
+        # self.remove_submission(unique_dir)
         return out.replace('\n', '<br>')
 
