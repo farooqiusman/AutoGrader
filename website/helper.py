f542e5af6292e7bc9ab4af9134e073f18e1b9617
farooqiusman <ufben10@gmail.com>
2023-02-09 21:23:45 -0500

    A3 almost done, need to check for output and calculate grade

--- website/helper.py
+++ website/helper.py
@@ -1,5 +1,26 @@
+from mysql.connector import connect, Error
 class static_methods:
-        
+    
+    @staticmethod
+    def remove_submission(subprocess, unique_dir): 
+        command = f"rm -rf {unique_dir}/"
+        subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    @staticmethod
+    def connect_to_sql(user, host, password, database):
+        cnx = None
+        try:
+            cnx = connect(
+                host = host,
+                user = user,
+                password = password,
+                database=database,
+            )
+            print("Database connection success")
+        except Error as e:
+            print(e)
+        return cnx
+
     @staticmethod
     def run_process(subprocess, command, directory):
         print(command)
@@ -21,5 +42,64 @@ class static_methods:
             out += f"Error running command: <code>{command}</code><br>{formatted_error}"
             return False, out
         out += stdout.decode('utf-8').replace("\n", "<br>")
+        return True, out
+    
+    @staticmethod
+    def run_cup(subprocess, command, directory):
+        print(command)
+        proc = subprocess.Popen(command, shell=True, cwd=directory, stdout=subprocess.PIPE)
+        out = ""
+        std = ""
+        try:
+            stdout = proc.communicate(timeout=15)[0]
+            std = stdout.decode('utf-8')
+            print(std)
+            if "0 errors" not in std:
+                raise Exception()
+        except subprocess.TimeoutExpired:
+            proc.kill()
+            out += f"Error running: <code>{command}</code> took too long to run. <br>"
+            return False, out
+        except:
+            out += f"Error running command: <code>{command}</code><br><code>{std}</code>"
+            return False, out.replace('\n', '<br>')
+        out += std
+        return True, out.replace('\n', '<br>')
+
+    @staticmethod
+    def run_code(subprocess, command, directory):
+        print(command)
+        proc = subprocess.Popen(command, shell=True, cwd=directory, stdout=subprocess.PIPE)
+        out = ""
+        std = ""
+        try:
+            stdout = proc.communicate(timeout=15)[0]
+            std = stdout.decode('utf-8')
+            print(std)
+            if "# Goto state #-1" not in std:
+                raise Exception()
+        except subprocess.TimeoutExpired:
+            proc.kill()
+            out += f"Error running: <code>{command}</code> took too long to run. <br>"
+            return False, out
+        except:
+            out += f"Error running command: <code>{command}</code><br><code>{std}</code>"
+            return False, out.replace('\n', '<br>')
+        out += std
+        return True, out.replace('\n', '<br>')
+
+    @staticmethod
+    def find_and_replace(text_to_find, text_to_replace, file_name):
+        out = ""
+        try:
+            with open(file_name, 'r') as f:
+                A3template = f.read()
+            
+            with open(file_name, 'w') as f:
+                f.write(A3template.replace(text_to_find, text_to_replace))
+        except Exception as e:
+            out += f'File error {e}<br>'
+            return False, out
+        
         return True, out
 
